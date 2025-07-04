46b1c06b6beed503bb81d3df066d15b3b4894ae8
R. Kukuh <rkukuh@gmail.com>
2025-07-04 07:44:04 +0700

    Add spinner animation to 'busy' button

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -5,7 +5,8 @@ export default {
                 'border rounded px-5 py-4 disabled:cursor-not-allowed': true,
                 'bg-blue-200': type === 'primary',
                 'bg-purple-200': type === 'secondary',
-                'bg-gray-200': type === 'muted'
+                'bg-gray-200': type === 'muted',
+                'spinner': is_loading
             }"
             :disabled="is_loading">
             <slot />
