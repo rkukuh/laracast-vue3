ed55578fc30918925a816b7d310649dc3ece0835
R. Kukuh <rkukuh@gmail.com>
2025-07-14 06:19:56 +0700

    Fix the redundant dynamic tags

--- js/components/TodoApp/TodoList.js
+++ js/components/TodoApp/TodoList.js
@@ -37,7 +37,7 @@ export default {
 
     computed: {
         tags() {
-            return this.tasks.map(task => task.tag);
+            return new Set(this.tasks.map(task => task.tag));
         }
     }
 }
